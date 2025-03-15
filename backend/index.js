3efe14de05cf040b3965353491d1c55a629f267a
branisted <branisted@gmail.com>
2025-03-15 19:30:00 +0200

    modified folder structure
    added backend and frontend

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend running on http://localhost:${PORT}`);
+});
